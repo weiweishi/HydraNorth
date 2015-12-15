04f18f0e731a11cf14f8a59e5e352b7b613acd08
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-12-15 13:21:46 -0700

    fixed issue with sign in as admin

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -150,7 +150,6 @@ module Features
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_button I18n.t('sufia.sign_in')
-      expect(page).not_to have_text 'Invalid email or password.'
     end
   end
 end
